7a3fbdaac1f262c6d99e3cf02c559f301a34e5f3
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2024-06-26 13:26:56 -0700

    remove timer repeat from script, use github actions instead

--- poll_meetinfo.py
+++ poll_meetinfo.py
@@ -1,5 +1,4 @@
 import requests
-import time
 import json
 import logging
 import asyncio
@@ -16,20 +15,16 @@ logging.basicConfig(filename='meetinfo.log', level=logging.INFO, format='%(ascti
 
 # Fetch Nostr private key from environment variable
 PRIVATE_KEY_HEX = os.getenv('NOSTR_PRIVATE_KEY_HEX')
-print(PRIVATE_KEY_HEX)
 if not PRIVATE_KEY_HEX:
     raise ValueError("The NOSTR_PRIVATE_KEY_HEX environment variable is not set.")
 
 # Initialize Nostr keys and signer
-#keys = Keys.from_private_key(PRIVATE_KEY_HEX)
 keys = Keys.parse(PRIVATE_KEY_HEX)
 signer = NostrSigner.keys(keys)
 
 # List of Nostr relay URLs
 NOSTR_RELAY_URLS = [
     'wss://testnet.plebnet.dev/nostrrelay/1',
-    # 'wss://relay.damus.io',
-    # 'wss://nos.lol'
 ]
 
 async def main():
@@ -44,33 +39,30 @@ async def main():
     await client.connect()
 
     url = "https://hivetalk.org/api/v1/meetinfo"
-    while True:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # Raise an error for bad responses
-            data = response.json()
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
 
-            if "meetings" in data:
-                for meeting in data["meetings"]:
-                    room_id = meeting["roomId"]
-                    peers = meeting["peers"]
-                    message = f"There are {peers} bee(s) now chatting in {room_id} on #HiveTalk. Join them now: https://hivetalk.org/join/{room_id}"
+        if "meetings" in data:
+            for meeting in data["meetings"]:
+                room_id = meeting["roomId"]
+                peers = meeting["peers"]
+                message = f"There are {peers} bee(s) now chatting in {room_id} on #HiveTalk. Join them now: https://hivetalk.org/join/{room_id}"
+                
+                # Check if the roomId has been announced in the last hour
+                if not was_announced_recently(room_id):
+                    # Log the message
+                    logging.info(message)
                     
-                    # Check if the roomId has been announced in the last hour
-                    if not was_announced_recently(room_id):
-                        # Log the message
-                        logging.info(message)
-                        
-                        # Post the message to Nostr relays
-                        await post_to_nostr(client, message)
+                    # Post the message to Nostr relays
+                    await post_to_nostr(client, message)
 
-            else:
-                logging.info("No meetings found.")
+        else:
+            logging.info("No meetings found.")
 
-        except requests.RequestException as e:
-            logging.error(f"An error occurred: {e}")
-
-        time.sleep(300)  # Wait for 5 minutes
+    except requests.RequestException as e:
+        logging.error(f"An error occurred: {e}")
 
 def was_announced_recently(room_id):
     with open('meetinfo.log', 'r') as log_file:
